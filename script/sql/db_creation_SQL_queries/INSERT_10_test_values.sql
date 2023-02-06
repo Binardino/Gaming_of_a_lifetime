807f7e050771790cbd191b4ef632a62a23e8f7c9
Robin Langlois <langlois.robin@gmail.com>
2023-02-06 11:45:01 +0100

    restructuration repo organisation

---
+++ script/sql/db_creation_SQL_queries/INSERT_10_test_values.sql
@@ -0,0 +1,13 @@
+INSERT INTO my_videogames
+VALUES
+(1, "Final Fantasy VIII", "PC","JRPG",True, 1998, 2000, 70, 91),
+(2, "Final Fantasy VII", "PS1","JRPG", True, 1997, 2018, 70, 98),
+(3, "Final Fantasy X", "PS2","JRPG", True, 2000, 2003, 150, 98),
+(4, "The Witcher 3", "PS4 | PC","RPG", True, 2015, 2018, 250, 100),
+(5, "Sonic 1", "Megadrive","Platformer", True, 1991, 1996, 40, 80),
+(6, "Sonic 2", "Megadrive","Platformer", True, 1992, 1996, 50, 85),
+(7, "Sonic 3", "Megadrive","Platformer", True, 1994, 1996, 40, 80),
+(8, "Sonic & Knuckles", "Megadrive","Platformer", True, 1994,1996, 20, 80),
+(9, "Sonic 3 & Knuckles", "Megadrive","Platformer", True, 1994,1996, 60, 90),
+
+(10, "Spiderman", "PS4", "Action | Platformer", True, 2018, 2020,30, 90);

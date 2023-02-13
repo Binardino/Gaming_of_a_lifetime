c8c459c58d244943a54cad84147c8210f3e4e8bb
Robin Langlois <langlois.robin@gmail.com>
2023-02-13 12:40:37 +0100

    add sq treemap & sns distplot

--- script/python/main.py
+++ script/python/main.py
@@ -16,6 +16,12 @@ df_vg = pd.read_csv('df_lifetime_gaming.csv')
 #calling add console function to add console brand label depending on console names
 df_consoles = cf.add_console_tag
 
+#%% Visualisation block
+### creating dict for general visualization settings 
+# sns.set_style(style='GnBu_d')
+sns.set(style = 'whitegrid', palette='deep', rc = {'figure.figsize':(20,10)}) 
+#figsize is not a param for sns.set BUT using rc & a dict, possible to add new params
+
 #%%**Treemap for each console amount of game played**
 
 # Using PyPlot dynamic Treemap to map, for each console and related brand, how many games have been played on each platform
@@ -24,6 +30,7 @@ df_consoles = cf.add_console_tag
 # - the Dynamic version (for online usage) with Hover effect - displaying information when hovering over the chart
 # - Static version with always on information (adding specific params) - version saved as PNG for local displaying
 
+# plt.figure(figsize=(20,10))
 px_treemap_graph(df=df_consoles,
                  ['brand', 'console'],
                  agg_func='count',
@@ -34,23 +41,29 @@ px_treemap_graph(df=df_consoles,
                  width=1000, height=750
                  )
 
-px_treemap_graph( )
+#%% squarify treemap for plotting game type
+sq_treemap_graph(df_size=dfga.values,
+                 df_value=dfga.values,
+                 label=dfga.index,
+                 colour=plt.cm.Dark2(np.random.rand(len(dfga.values))),
+                 title='Treemap version - Amount of game types played - all consoles combined'
+                 image_path='../Plots_Charts_PNG/treemap_game_type.png'
+                 )
 
-#%%
-#ion with always on information (adding specific params) - version saved as PNG for local displaying
+#%% **Distplot to measure the distribution of hours played by game**
+# 
+# I consider a game to be a good one whenever I spend more than 15-20 hours on it.
+# Especially when I pay full price for a game, I expect it to be at least 30-40 hours long, if not I consider it a scam.
+# 
+# Below distplot illustrates I spent in general between 15 & 30 for most of the games I played
 
-# plt.figure(figsize=(20,10))
-fig_console = px.treemap(data_frame=df_consoles, 
-                         path=['brand', 'console'], 
-                         values='count', 
-                         color='brand',
-                         color_discrete_map={'PlayStation' : '#0D0BDE', 'Microsoft' :'#008D00', 'Nintendo': '#C90104' , 
-                                             'Sega':'#d787ff', 'Android':'#3DDC84'},
-                         title='Amount of game played per consoles - organized per console brand',
-                         width=1000, height=750
-                        )
-#adding param for always displaying information for saving locally PNG image without hover effect
-fig_console.data[0].textinfo = 'label+text+value'
-# fig_console.layout.hovermode = False
-fig_console.write_image('../Plots_Charts_PNG/console_distribution.png')
-fig_console.show()
+# In[49]:
+
+
+ax = sns.distplot(df_vg['hours_played'], bins=50)
+
+plt.title('Distribution of hours played per game', fontsize=15)
+ax.xaxis.set_major_locator(ticker.MultipleLocator(15)) #setting xticks to 15
+
+plt.savefig(('../Plots_Charts_PNG/distplot_hours_played_all_game.png'))
+plt.show()

a5d724e9bc9ce39c21849bd8078b0e2cd1ba9d5b
Robin Langlois <langlois.robin@gmail.com>
2023-02-13 12:49:45 +0100

    add string cleaning in data wrangling function

--- script/python/functions/data_wrangling.py
+++ script/python/functions/data_wrangling.py
@@ -9,6 +9,14 @@ import pandas as pd
 import numpy as np
 
 #%%
+def str_cleaning(df):
+    """cleaning str in columns to perform further analysis"""
+    df['game_type'] = df['game_type'].str.replace(' / ', '', regex=False)
+    df['console'] = df['console'].str.replace(' / ', '', regex=False)
+
+    return df
+
+#%%
 def add_console_tag(df):
     """
     add console brand depending on name of console using custom condition list

2acacdeaf350985a66f013f95a1c87c9a323bf77
Robin Langlois <langlois.robin@gmail.com>
2023-05-08 12:44:49 +0200

    add function create_console_list

--- script/python/functions/data_wrangling.py
+++ script/python/functions/data_wrangling.py
@@ -13,8 +13,17 @@ def str_cleaning(df):
     """cleaning str in columns to perform further analysis"""
     df['game_type'] = df['game_type'].str.replace(' / ', '', regex=False)
     df['console'] = df['console'].str.replace(' / ', '', regex=False)
-
+    
     return df
+
+#%%
+def create_console_list(df):
+    df_console_raw = pd.DataFrame(df['console'])
+    df_console_raw['console'] = df_console_raw['console'].str.split(pat='|')
+    df_console_raw = df_console_raw.explode('console').reset_index(drop=True)
+    console_list = df_console_raw['console'].unique().tolist()
+
+    return df_console_raw, console_list
 
 #%%
 def add_console_tag(df):

aad1797e830f6da27ce70038de4c2ee719322b2d
Robin Langlois <langlois.robin@gmail.com>
2023-06-28 13:55:53 +0200

    add parser for metacrtic soup fetch

--- app/functions/metacritic_scraper.py
+++ app/functions/metacritic_scraper.py
@@ -28,11 +28,18 @@ class HTMLRequests_metacritic:
 
 class metacritic_data_fetcher:
     def create metacritic_dict(soup):
+    #fetch data from tableclam html element
     game_rows = soup.select('tableclam-list tr')
 
     game_dict = {}
 
+    # Iterate over each game row from soup element and extract the required information
     for row in game_rows:
-
-    return gameè
+        game_title = row.select_one('.title').text.strip() if row.select_one('.title') else None
+        game_score = row.select_one('.metascore_w').text.strip() if row.select_one('.metascore_w') else None
+        game_user_score = row.select_one('.clamp-userscore').text.strip() if row.select_one('.clamp-userscore') else None
+        game_platform = row.select_one('.platform .data').text.strip() if row.select_one('.platform .data') else None
+        game_release_date = row.select_one('.clamp-details').text.strip() if row.select_one('.clamp-details') else None
+        game_summary = row.select_one('.summary').text.strip() if row.select_one('.summary') else None
+        
     

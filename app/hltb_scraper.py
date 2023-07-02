931a6ceda56fe7e6eac4ad14612045fc1916fd6a
Robin Langlois <langlois.robin@gmail.com>
2023-07-02 14:42:44 +0200

    add create payload function

--- app/hltb_scraper.py
+++ app/hltb_scraper.py
@@ -13,6 +13,43 @@ class HLTBRequests_post:
             }
         return headers
 
+    def create_payloads(game_name):
+        
+        payload_raw = {
+                    'searchType': "games",
+                    'searchTerms': game_name.split(),
+                    'searchPage': 1,
+                    'size': 20,
+                    'searchOptions': {
+                        'games': {
+                            'userId': 0,
+                            'platform': "",
+                            'sortCategory': "popular",
+                            'rangeCategory': "main",
+                            'rangeTime': {
+                                'min': 0,
+                                'max': 0
+                            },
+                            'gameplay': {
+                                'perspective': "",
+                                'flow': "",
+                                'genre': ""
+                            },
+                            'modifier': '',
+                        },
+                        'users': {
+                            'sortCategory': "postcount"
+                        },
+                        'filter': "",
+                        'sort': 0,
+                        'randomizer': 0
+                    }
+                }
+        
+        payload = json.dumps(payload_raw)
+        
+        return payload
+    
     def get_hltb_game_data(game_list):
     url = 'https://howlongtobeat.com/api/search'
     

bc5618434ae9da59c40f98c347e98ec005e7ec66
Robin Langlois <langlois.robin@gmail.com>
2023-07-02 14:40:42 +0200

    add class creation

--- app/hltb_scraper.py
+++ app/hltb_scraper.py
@@ -2,20 +2,25 @@ import requests
 from fake_useragent import UserAgent
 import json
 
-url = 'https://howlongtobeat.com/api/search'
 
+class HLTBRequests_post:
+    def create_headers():
+        ua = UserAgent()
+        headers = {'Accept-Language': 'en-US,en;q=0.8',
+                   'Upgrade-Insecure-Requests': '1',
+                   'content-type': 'application/json',
+                   'User-Agent': ua.random.strip()
+            }
+        return headers
 
-game_list = df_vg['game_name'].sample(10)
-
-game_dict = {}
-for game_name in game_list:
-#game_name = 'Horizon Forbidden West'
+    def get_hltb_game_data(game_list):
+    url = 'https://howlongtobeat.com/api/search'
     
-    ua = UserAgent()
-    headers = {'Accept-Language': 'en-US,en;q=0.8',
-           'Upgrade-Insecure-Requests': '1',
-           'content-type': 'application/json',
-           'User-Agent': ua.random.strip(),
-           'referer': url
-    }
     
+    game_list = df_vg['game_name'].sample(10)
+    
+    game_dict = {}
+    for game_name in game_list:
+    #game_name = 'Horizon Forbidden West'
+        
+        headers = create_headers()

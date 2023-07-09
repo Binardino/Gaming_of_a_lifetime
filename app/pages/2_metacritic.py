497a2d16f359951d9b848f980fba87c291e4d589
Robin Langlois <langlois.robin@gmail.com>
2023-07-09 18:23:48 +0200

    add markdown 101

--- app/pages/2_metacritic.py
+++ app/pages/2_metacritic.py
@@ -16,8 +16,7 @@ from app.functions.data_wrangling import *
 from app.functions.metacritic_wrangling import *
 from app.functions.visualisation_tools import *
 
-st.set_page_config(page_title="page2")
-#%% import data
+#%%#%% import data
 #@st.cache_data
 def get_data_csv(path):
     return pd.read_csv(path)
@@ -31,6 +30,22 @@ df_vg = get_data_csv('../df_vg_local_csv.csv')
 df_meta = get_data_csv('../../db_data/csv/metacritic_6900_games_22_Dec_2016_updated.csv')
 st.write(df_vg)
 
+
+st.set_page_config(page_title="page2")
+#%% README
+st.write("# Welcome to the Adventure of a Lifetime - Metacritic comparison🎮")
+
+st.markdown("""The goal of this part is to compare personal data from my videogame_lifetime database with the data from Metacritic.
+            
+            **Compare data from Metacritic dataset**
+            
+            The available Metacritic dataset dates back from 2016;
+            
+            Remaing data past 2016 fetched through web scraping from metacritic webpage for set of consoles I played.
+            Side notebook updated the 2016 dataset used & displayed here. Only the {df_vg.size}
+            
+            """)
+#%% import data
 df_meta['Name'].fillna('NaN', inplace=True)
 # df_meta['fuzz'] = df_meta['Name'].apply(lambda x : fuzzymatch_metacritic(x, df_vg))
 

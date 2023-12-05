a7725e4b327abaaac5fad0ff7dcc5729e15f142b
Robin Langlois <langlois.robin@gmail.com>
2023-12-05 14:29:55 +0100

    add 2023 new games

--- db_data/queries/UPDATE_NEW_games_2023.sql
+++ db_data/queries/UPDATE_NEW_games_2023.sql
@@ -22,12 +22,16 @@ game_name = "Demon's Souls";
 ###2022 - 2023 updates
 INSERT INTO gaming_lifetime(game_name, console, game_type, finished, published_year, played_year, hours_played, perso_score, multiplayed) 
 VALUES
-("Marvel's Spider-Man: Miles Morales", "PS5", "Open World|Action-Adventure", False, 2020,2022, 20,89,False),
+("Marvel's Spider-Man: Miles Morales", "PS5", "Open World|Action-Adventure", True, 2020,2022, 20,87,False),
+("Marvel's Spider-Man 2", "PS5", "Open World|Action-Adventure", True, 2023,2023, 22,89,False)
 ("Kena: Bridge of Spirits", "PS5", "RPG|Action-Adventure", False, 2021,2023, 15,89,False),
-("Ghost of Tsushima", "PS5", "Open World|Action-Adventure", False, 2020,2023, 15,91,False),
-("God of War: Ragnarök", "PS5", "RPG|Action-Adventure", False, 2023,2023, 45,97,False),
+("Ghost of Tsushima", "PS5", "Open World|Action-Adventure", False, 2020,2023, 15,92,False)
 ("The Legend of Zelda: Tears of the Kingdom", "Switch", "Open World|Action-Adventure", False, 2023,2023, 15,99,True),
-("God of War: Ragnarök", "PS5", "RPG|Action-Adventure", False, 2023,2023, 45,97,False),
+("God of War: Ragnarök", "PS5", "RPG|Action-Adventure", False, 2023,2023, 45,97,False),,
+("God of War", "PS2", "Beat'em all", False, 2002,2023, 10,79,False),
 ("The Last of Us Part I", "PS5", "Survival Horor|Action-Adventure", True, 2013,2023, 23,97,True),
 ("The Last of Us Part II", "PS5", "Survival Horor|Action-Adventure", True, 2020,2023, 30,97,True),
 ("A Way Out", "PS5", "Co-op", False, 2023,2023, 45,97,True),
+("Hogwarts Legacy", "PS5", "Open World|Action-Adventure", False, 2023,2023,50,92,True),
+("Fifa 2022", "PS5", "Sport", False, 2022,2022,15,80,True),
+("Bloodborne", "PS5", "JRPG|Action-Adventure", False, 2015,2023,5,85,True)

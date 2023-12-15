ff4769032b522ad896e1e710ff150e813e46b601
Robin Langlois <langlois.robin@gmail.com>
2023-12-15 21:45:21 +0100

    add co func

--- app/functions/db_connection.py
+++ app/functions/db_connection.py
@@ -8,11 +8,18 @@ script for SQL connection
 """
 
 #%%importing libraries
+import pandas as pd
 from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv
 load_dotenv()
+#%%%
+def get_data_csv(path):
+    return pd.read_csv(path)
 
+#st.cache_data
+def get_data_sql(sql, engine):
+    return pd.read_sql(sql=sql, con=engine)
 #%%functions
 def sql_connection():
     driver   = os.environ.get("driver")

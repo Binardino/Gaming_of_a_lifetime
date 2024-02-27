5302f016cc868fde2f59646f967aa8b88706821b
Robin Langlois <langlois.robin@gmail.com>
2024-02-27 12:26:07 +0100

    delete commented bloc - erase computing from this page to make it on other script local_metacritic

--- app/pages/2_metacritic.py
+++ app/pages/2_metacritic.py
@@ -44,35 +44,6 @@ df_meta = db_co.get_data_sql(sql=query, engine=engine_vg.connect())
 query = sqlalchemy.text('SELECT * FROM gaming_lifetime')
 df_vg = db_co.get_data_sql(sql=query, engine=engine_vg.connect())
 #st.write(df_vg)
-#%% import data
-
-df_meta['game_name'].fillna('NaN', inplace=True)
-# df_meta['fuzz'] = df_meta['Name'].apply(lambda x : fuzzymatch_metacritic(x, df_vg))
-
-#with tqdm(total=df_vg.shape[1]) as pbar:
-tqdm.pandas()
-df_vg['fuzz'] = df_vg['game_name'].apply(lambda x : fuzzymatch_metacritic(x, df_meta['game_name']))
-#    pbar.update(1)
-
-# st.write(df_meta)
-st.write(df_vg)
-
-df_merge = pd.merge(df_vg, df_meta, 
-                    how='inner', 
-                    left_on='fuzz', 
-                    right_on='game_name', 
-                    suffixes=['', '_y']
-                    )
-
-st.write(df_merge)
-
-fig_scatter = px.scatter(data_frame=df_merge,
-                         x='perso_score',
-                         y='critic_score',
-                         color='console',
-                         hover_name='game_name')
-
-st.plotly_chart(fig_scatter)
 
 #%% correlation between scores
 st.write("""I want to establish, for both my personal scores and the one from Metacritic,

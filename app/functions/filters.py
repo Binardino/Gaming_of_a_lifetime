f660c5ba4074803042dd3cf2b0ce935e08a0d097
Binardino <langlois.robin@gmail.com>
2025-12-19 13:00:27 +0100

    clean functions def param input

--- app/functions/filters.py
+++ app/functions/filters.py
@@ -1,26 +1,26 @@
 import pandas as pd
-fro; typing import Dict, List, Tuple, Any
+from typing import Dict, List, Tuple, Any
 
 filters = Dict[str, Any]
 
-def filter_range(df : pd.DataFrame,
-                 column : str,
+def filter_range(df          : pd.DataFrame,
+                 column      : str,
                  value_range : Tuple[int,int]
                  ) -> pd.DataFrame:
     """Filter df by range of numeric values"""
     min_val, max_val = value_range
     return df[df[column].between(min_val, max_val)]
 
-def filter_exact(df: pd.DataFrame,
-                 column : str,
-                 values : List[str]
+def filter_exact(df          : pd.DataFrame,
+                 column      : str,
+                 values      : List[str]
                  ) -> pd.DataFrame:
     """ Filter df by category column """
     return df[df[column].isin(values)]
 
-def filter_mapping(df : pd.DataFrame, 
-                   column : str,
-                   values : List[str],
+def filter_mapping(df           : pd.DataFrame,
+                   column       : str,
+                   values       : List[str],
                    mapping_dict : Dict[str, List[str]]
                 ) -> pd.DataFrame:
     """ Handle columns with concatenated values (e.g. console : ['PC|PS4']) """
